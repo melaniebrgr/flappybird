89553c28515df7606f3522f0ee24e4cb160d9baf
Melanie Burger <melanie.brgr@gmail.com>
2016-04-02 20:20:19 -0400

    Defer scripts

--- site/js/main.js
+++ site/js/main.js
@@ -1,35 +1,36 @@
+$(function() {
+	// Load below-the-fold CSS
+	// the CSS will automatically execute when loaded
+	// (function loadCSS(){
+	//     $('head').append('<link rel="stylesheet" href="css/styles.css">');
+	// })();
 
-// Load below-the-fold CSS
-// the CSS will automatically execute when loaded
-(function loadCSS(){
-    $('head').append('<link rel="stylesheet" href="css/styles.css">');
-})();
-
-//Animation
-// Fade-in/up game features
-$('.features li').each(function(i, el) {
-	$(el).velocity({
-		opacity: 1,
-		translateY: "-20px",
-	}, {
-		duration: 500,
-		delay: i*250,
-		easing: "easeOutQuad"
+	//Animation
+	// Fade-in/up game features
+	$('.features li').each(function(i, el) {
+		$(el).velocity({
+			opacity: 1,
+			translateY: "-20px",
+		}, {
+			duration: 500,
+			delay: (i*250),
+			easing: "easeOutQuad"
+		});
 	});
-});
 
-// Wiggle CTA after delay
-$('.call-to-action .button').velocity({
-	rotateZ: 4
-}, {
-	duration: 100,
-	delay: 4000
-}).velocity({
-	rotateZ: -4
-}, {
-	duration: 100
-}).velocity("reverse").velocity({
-	rotateZ: 0
-}, {
-	duration: 100
+	// Wiggle CTA after delay
+	$('.call-to-action .button').velocity({
+		rotateZ: 4
+	}, {
+		duration: 100,
+		delay: 4000
+	}).velocity({
+		rotateZ: -4
+	}, {
+		duration: 100
+	}).velocity("reverse").velocity({
+		rotateZ: 0
+	}, {
+		duration: 100
+	});
 });
